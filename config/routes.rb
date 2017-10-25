2ed571f6025f83a49f52962c7a691b969be05843
aisayo <aisayo@icloud.com>
2017-10-25 17:03:58 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post '/authors', to: "authors#create"
 
   get "/posts/:id", to: "posts#show", as: "post"
-  get "/posts/:id/edit", to: "posts#edit", as: "edit_author"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
   patch 'posts/:id', to: 'posts#update'
 
   # The priority is based upon order of creation: first created -> highest priority.
